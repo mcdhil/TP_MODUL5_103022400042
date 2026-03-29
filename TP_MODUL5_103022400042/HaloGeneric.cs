174ccd5081f4f008bd2274c47cd8d41feb5644fd
MASAGUS AHMAD FADHIL <masagusahmadfadhil@student.telkomuniversity.ac.id>
2026-03-30 00:47:52 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ TP_MODUL5_103022400042/HaloGeneric.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class HaloGeneric
+{
+    // Method dengan generic parameter T
+    public void SapaUser<T>(T X)
+    {
+        Console.WriteLine($"Halo user {X}");
+    }
+}
--- TP_MODUL5_103022400042/Program.cs
+++ TP_MODUL5_103022400042/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TP_MODUL5_103022400042
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HaloGeneric sapaan = new HaloGeneric();
+            // Input nama praktikan
+            String nama = "Fadhil";
+            sapaan.SapaUser(nama);
+        }
+    }
+}
