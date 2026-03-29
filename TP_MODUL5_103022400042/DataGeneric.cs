ba36ce6857888d114875e1cf6a43467661d787af
MASAGUS AHMAD FADHIL <masagusahmadfadhil@student.telkomuniversity.ac.id>
2026-03-30 00:51:20 +0700

    menambahkan class DataGeneric

---
+++ TP_MODUL5_103022400042/DataGeneric.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class DataGeneric<T>
+{
+    private T data;
+
+    // Konstruktor
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {this.data}");
+    }
+}
--- TP_MODUL5_103022400042/Program.cs
+++ TP_MODUL5_103022400042/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TP_MODUL5_103022400042
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input menggunakan NIM 
+            String nim = "130220XXXX"; // Ganti dengan NIM kamu
+            DataGeneric<String> dataNIM = new DataGeneric<String>(nim);
+            dataNIM.PrintData();
+        }
+    }
+}
